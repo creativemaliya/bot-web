2442cdbd2cc560c1ee963070e56ff3ecffbfa6ee
creativemaliya <creativemaliya@gmail.com>
2025-12-12 22:25:55 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *create by maleesha nethsara* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://raw.githubusercontent.com/creativemaliya/photo/refs/heads/main/images.jpeg",
               },
               caption: sid,
             });
@@ -129,4 +129,5 @@ process.on("uncaughtException", function (err) {
 });
 
 module.exports = router;
+
 
